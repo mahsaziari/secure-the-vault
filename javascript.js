6b397647eb12dbe0ced0cd6ff728ada5f0016bf6
Mahsa Ziari <mahsaz3@hotmail.com>
2024-02-01 18:00:25 -0800

    add index.html

---
+++ javascript.js
@@ -0,0 +1,15 @@
+const x = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+console.log("You have received this message because you have been chosen to open an important vault. Here is the secret combination:");
+// string to notify user
+var num = 5 + 5;
+console.log(num);
+var num = 8 * 5;
+console.log(num);
+var num = 45 - 6;
+console.log(num);
+// arithmetic operations that equal the combination
+let str1 = 10;
+let str2 = 40;
+let str3 = 39;
+alert(str1 + " " + str2 + " " + str3);
+// Concatenate strings using variables to display over dialogue box
